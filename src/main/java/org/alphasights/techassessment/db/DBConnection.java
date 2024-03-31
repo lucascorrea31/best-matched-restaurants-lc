27c55959299aeb1dcf1effe9fb96135bad68b032
Lucas Corrêa <lucas.nhl@gmail.com>
2024-03-31 18:52:15 -0300

    feat: create db connection

---
+++ src/main/java/org/alphasights/techassessment/db/DBConnection.java
@@ -0,0 +1,32 @@
+package org.alphasights.techassessment.db;
+
+
+import org.alphasights.techassessment.util.Constants;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnection {
+    private static final Logger LOGGER =
+            Logger.getLogger(DBConnection.class.getName());
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            String url = Constants.DB_URL;
+            String user = Constants.DB_USER;
+            String password = Constants.DB_PASSWORD;
+
+            try {
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException ex) {
+                LOGGER.log(Level.SEVERE, "Connection error", ex);
+            }
+        }
+
+        return connection;
+    }
+}
---
+++ src/main/java/org/alphasights/techassessment/util/Constants.java
@@ -0,0 +1,10 @@
+package org.alphasights.techassessment.util;
+
+public class Constants {
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/best_match_db";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASSWORD = "1234";
+    public static final String CUISINE_TABLE = "cuisines";
+    public static final String RESTAURANT_TABLE = "restaurants";
+
+}
